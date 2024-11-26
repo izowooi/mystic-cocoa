b59acf8dddd3974b2ec91100c60dce5d9f8a2323
jongwoo <izowooi@hotmail.com>
2024-11-26 23:46:34 +0900

    올해의 운세 위젯 최초 추가

---
+++ flutter_proj/lib/widget/yearly_widget.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_flip_card/flutter_flip_card.dart';
+import 'package:flutter_proj/controller/tarot_data_controller.dart';
+import 'tarot_select_widget.dart';
+
+class YearlyWidget extends ConsumerStatefulWidget {
+  YearlyWidget({Key? key}) : super(key: key);
+
+  @override
+  _YearlyWidgetState createState() => _YearlyWidgetState();
+}
+
+class _YearlyWidgetState extends ConsumerState<YearlyWidget> {
+  List<String> cardIndex = List.generate(
+    24,
+    (index) => index.toString().padLeft(2, '0'),
+  );
+
+  List<FlipCardController> controllers = List.generate(24, (_) => FlipCardController());
+
+  @override
+  void initState() {
+    super.initState();
+    cardIndex.shuffle(); // 초기화 시 리스트를 섞습니다.
+  }
+
+  void shuffleImages() async{
+    for (var controller in controllers) {
+      if (controller.state?.isFront == false) {
+        await controller.flipcard(); // 플립된 카드를 원래대로 되돌립니다.
+      }
+    }
+    setState(() {
+      cardIndex.shuffle();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      overrides: [
+        interpretationProvider.overrideWithValue((String index) => TarotDataController().getYearlyInterpretation(index)),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('MysticCocoa'),
+        ),
+        body: TarotSelectWidget(
+          cardIndex: cardIndex,
+          controllers: controllers,
+          onShuffle: shuffleImages,
+        ),
+      ),
+    );
+  }
+}
