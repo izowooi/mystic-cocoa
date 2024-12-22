bd67037f788f4e0e2f2ad042121865f43e3aec2d
jongwoo <izowooi@hotmail.com>
2024-12-22 21:29:22 +0900

    fcm 서버 코드 최초 추가

--- python_proj/main.py
+++ python_proj/main.py
@@ -1,37 +1,34 @@
-print('hello world')
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+# Firebase Admin SDK 초기화
+cred = credentials.Certificate("../secure/firebase-adminsdk.json")
+firebase_admin.initialize_app(cred)
 
 
-def process_file(input_file, output_file):
-    with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
-        result_lines = []  # 최종 결과를 저장할 리스트
-        current_card = []  # "오늘의 카드:"를 포함한 현재 카드 내용
+# FCM 메시지 전송 함수
+def send_fcm_message(notification_title, notification_text, notification_image, notification_topic):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=notification_title,
+            body=notification_text,
+            image=notification_image
+        ),
+        topic=notification_topic,
+    )
 
-        for line in infile:
-            # 1. 따옴표 치환
-            line = line.replace('"', '\\"')
-            # 2. 탭 치환
-            line = line.replace("\t", " ")
-            # 3. "오늘의 카드:" 확인
-            if "오늘의 카드:" in line:
-                # 이전 카드 내용이 있으면 합쳐서 저장
-                if current_card:
-                    result_lines.append("".join(current_card) + "\n")
-                    current_card = []
-                # 새로운 카드 시작
-                current_card.append(line.strip())
-            else:
-                # "오늘의 카드:"가 없으면 맨 앞에 "\n" 추가
-                current_card.append("\\n" + line.strip())
-
-        # 마지막 카드 처리
-        if current_card:
-            result_lines.append("".join(current_card) + "\n")
-
-        # 4. 결과를 합치고 파일로 저장
-        outfile.writelines(result_lines)
+    try:
+        # FCM 메시지 전송
+        response = messaging.send(message)
+        print(f"Successfully sent message: {response}")
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
 
-# 실행 예제
-input_file = "/Users/izowooi/Downloads/temp/input.txt"  # 입력 파일 경로
-output_file = "/Users/izowooi/Downloads/temp/output.txt"  # 출력 파일 경로
-process_file(input_file, output_file)
+# 함수 실행
+if __name__ == "__main__":
+    notification_title = "titlew"
+    notification_text = "textw"
+    notification_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Tarockkarten_in_der_Hand_eines_Spielers.jpg/440px-Tarockkarten_in_der_Hand_eines_Spielers.jpg"
+    notification_topic = "morning_utc9_kr"
+    send_fcm_message(notification_title, notification_text, notification_image, notification_topic)
