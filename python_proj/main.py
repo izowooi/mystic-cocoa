dbaae1fdb3b7442c54d6a797d31ddbf9c0cf4f45
jongwoo <izowooi@hotmail.com>
2024-12-24 22:06:37 +0900

    5개 국어 및 time zone 관련 기능 최초 추가

--- python_proj/main.py
+++ python_proj/main.py
@@ -1,7 +1,10 @@
+import json
 import os
 import firebase_admin
 from firebase_admin import credentials, messaging
 from datetime import datetime
+import random
+
 
 
 # Firebase Admin SDK 초기화
@@ -40,19 +43,21 @@ def send_fcm_message(notification_title, notification_text, notification_image, 
 
 
 # 시간에 따른 UTC 토픽 계산
-def calculate_utc_topics(current_hour):
-    ret = []
+def convert_hour_to_utc(current_hour):
+    ret_utc_offset = []
     # UTC 오프셋 계산
     utc_offset = current_hour + 1
     if utc_offset > 14:
         utc_offset -= 24
-    ret.append(f"morning_utc{utc_offset}_kr")
+    #ret.append(f"morning_utc{utc_offset}_kr")
+    ret_utc_offset.append(utc_offset)
 
     if 11 <= current_hour <= 13:
         utc_offset -= 24
-        ret.append(f"morning_utc{utc_offset}_kr")
+        #ret.append(f"morning_utc{utc_offset}_kr")
+        ret_utc_offset.append(utc_offset)
 
-    return ret
+    return ret_utc_offset
 
 
 
@@ -68,8 +73,32 @@ if __name__ == "__main__":
     notification_title = "title"
     notification_text = "text"
     notification_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Tarockkarten_in_der_Hand_eines_Spielers.jpg/440px-Tarockkarten_in_der_Hand_eines_Spielers.jpg"
-    notification_topics = calculate_utc_topics(current_hour)
+    utc_offsets = convert_hour_to_utc(current_hour)
+    valid_language = ["ko", "en", "jp", "zh", "ar"]
+    title_dict = {}
+    message_dict = {}
+    random_num = f"{random.randint(0, 19):02}"
 
-    for notification_topic in notification_topics:
-        print(f"Sending FCM to topic: {notification_topic}")
-        #send_fcm_message(notification_title, notification_text, notification_image, notification_topic)
+    #   valid_language 의 내용을 순회하면서 fcm_message_{language}.json 파일을 읽어서 title_dict, message_dict에 저장
+    #이 때 키값은 language 가 된다.
+    for language in valid_language:
+        with open(f"fcm_title_{language}.json", "r", encoding="utf-8") as infile:
+            data = json.load(infile)
+            title_dict[language] = data
+
+        with open(f"fcm_message_{language}.json", "r", encoding="utf-8") as infile:
+            data = json.load(infile)
+            message_dict[language] = data
+
+    for language in valid_language:
+        print(f"language: {language}, title: {title_dict[language][random_num]}, message: {message_dict[language]["00"]}")
+    #
+    #
+    # for utc_offset in utc_offsets:
+    #     for language in valid_language:
+    #         topic = f"aos_utc{utc_offset}_{language}"
+    #         title_text = title_dict[language][random_num]
+    #         message_text = message_dict[language][random_num]
+    #         print(f"utc_offset:{utc_offset}, topic: {topic}, title: {title_text}, message: {message_text}")
+    #         #"aos_utc{utc_offset}_kr")
+    #         #send_fcm_message(notification_title, notification_text, notification_image, notification_topic)
